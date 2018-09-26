c6f12d79beafc29ecf0855e9d83542bfc7985412
Kouhei Sano <ratatatta6@gmail.com>
2018-09-26 23:22:01 +0900

    楽天ブックスの洋書データを持ってこれるようになった

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import urllib.request
+import json
 
-def main():
-    print("hello 2")
+import constants
 
 if __name__ == '__main__':
-    main()
+
+    # rakuten URL
+    url = "https://app.rakuten.co.jp/services/api/BooksForeignBook/Search/20170404"
+    params = {
+        'format': 'json',
+        'booksGenreId': '005407',
+        'applicationId': constants.APPLICATION_ID,
+        'title': 'Potter',
+        'hits': 1,
+    }
+
+    request = urllib.request.Request('{}?{}'.format(url, urllib.parse.urlencode(params)))
+    response = urllib.request.urlopen(request)
+
+    # booksDataDict.__class__.__name__ => dict
+    # データ構造については以下を参照
+    # https://webservice.rakuten.co.jp/api/booksforeignbooksearch/#outputParameter
+    booksDataDict = json.loads(response.read().decode('utf8'))
+    print(booksDataDict.keys())
