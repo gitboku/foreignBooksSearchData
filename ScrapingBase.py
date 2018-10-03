cb186a6ab57b6d2c3bfdfb27219872294b9dc4d4
Kouhei Sano <ratatatta6@gmail.com>
2018-10-03 23:24:37 +0900

    mainからいらないところを抜き出し、文字コード設定

--- ScrapingBase.py
+++ ScrapingBase.py
@@ -17,14 +17,14 @@ class ScrapingBase:
         'hits': 1,
     }
 
-    request = urllib.request.Request('{}?{}'.format(url, urllib.parse.urlencode(params)))
-    response = urllib.request.urlopen(request)
+    # request = urllib.request.Request('{}?{}'.format(url, urllib.parse.urlencode(params)))
+    # response = urllib.request.urlopen(request)
 
-    # booksDataDict.__class__.__name__ => dict
-    # データ構造については以下を参照
-    # https://webservice.rakuten.co.jp/api/booksforeignbooksearch/#outputParameter
-    booksDataDict = json.loads(response.read().decode('utf8'))
-    print(booksDataDict.keys())
+    # # booksDataDict.__class__.__name__ => dict
+    # # データ構造については以下を参照
+    # # https://webservice.rakuten.co.jp/api/booksforeignbooksearch/#outputParameter
+    # booksDataDict = json.loads(response.read().decode('utf8'))
+    # print(booksDataDict.keys())
 
     # 楽天ブックス総合検索APIから書籍の情報を得る
     def getBookInfoFromRakuten(self):
