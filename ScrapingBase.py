afc983a1c737f1cb96b2308059ed3174bb22dfc5
Kouhei Sano <ratatatta6@gmail.com>
2018-10-10 23:58:16 +0900

    楽天APIにアクセスするためのクラスを作成

--- ScrapingBase.py
+++ ScrapingBase.py
@@ -8,25 +8,6 @@ import constants
 
 class ScrapingBase(metaclass=ABCMeta):
 
-    # rakuten URL
-    url = "https://app.rakuten.co.jp/services/api/BooksForeignBook/Search/20170404"
-    params = {
-        'format': 'json',
-        'booksGenreId': '005407',
-        'applicationId': constants.APPLICATION_ID,
-        'title': 'Potter',
-        'hits': 1,
-    }
-
-    # request = urllib.request.Request('{}?{}'.format(url, urllib.parse.urlencode(params)))
-    # response = urllib.request.urlopen(request)
-
-    # # booksDataDict.__class__.__name__ => dict
-    # # データ構造については以下を参照
-    # # https://webservice.rakuten.co.jp/api/booksforeignbooksearch/#outputParameter
-    # booksDataDict = json.loads(response.read().decode('utf8'))
-    # print(booksDataDict.keys())
-
     # 書籍情報をスクレイピングする
     # スクレイピング結果は配列で返す
     @abstractmethod
