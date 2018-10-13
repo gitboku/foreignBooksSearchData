90aaea6fb8087e8801b1c4ebfe653c8ea5c3f433
Kouhei Sano <ratatatta6@gmail.com>
2018-10-13 11:24:32 +0900

    楽天ブックス総合検索APIから情報を取得

--- RakutenApi.py
+++ RakutenApi.py
@@ -1,24 +1,37 @@
 # coding: UTF-8
 
 import constants
+import urllib.request
+import json
+
+import types
 
 class RakutenApi():
 
     # rakuten URL
-    url = "https://app.rakuten.co.jp/services/api/BooksForeignBook/Search/20170404"
+    url = "https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404"
     params = {
         'format': 'json',
-        'booksGenreId': '005407',
+        'booksGenreId': '000',
         'applicationId': constants.APPLICATION_ID,
-        'title': 'Potter',
-        'hits': 1,
+        'isbnjan': 9784794604545
     }
 
-    # request = urllib.request.Request('{}?{}'.format(url, urllib.parse.urlencode(params)))
-    # response = urllib.request.urlopen(request)
+    def getBookInfoWithIsbn(self, isbn):
+        request = urllib.request.Request('{}?{}'.format(self.url, urllib.parse.urlencode(self.params)))
+        response = urllib.request.urlopen(request)
 
-    # # booksDataDict.__class__.__name__ => dict
-    # # データ構造については以下を参照
-    # # https://webservice.rakuten.co.jp/api/booksforeignbooksearch/#outputParameter
-    # booksDataDict = json.loads(response.read().decode('utf8'))
-    # print(booksDataDict.keys())
+        # booksDataDict.__class__.__name__ => dict
+        # データ構造については以下を参照
+        # https://webservice.rakuten.co.jp/api/booksforeignbooksearch/#outputParameter
+        booksDataDict = json.loads(response.read().decode('utf8'))
+        itemSet = booksDataDict['Items']
+
+        testItem = itemSet[0]
+        testItem = testItem['Item']
+        
+        for key, value in testItem.items():
+            if type(value) == int:
+                print(key + ': ' + str(value))
+            else:
+                print(key + ': ' + value)
